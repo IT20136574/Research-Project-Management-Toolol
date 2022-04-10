c13c7389029be6fa738f0c0ed6fb6acec2d000c4
Dimalka Rathnayake <89465728+IT20128418@users.noreply.github.com>
2022-04-10 22:39:24 +0530

    registration

---
+++ project_backend/routes/DH_routes/student.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = require("express").Router();
+let student = require("../../models/DH_models/student");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
+
+
+
+
+//sign up
+
+router.post("/signup", async (req, res) => {
+    try {
+      const {
+        name,
+        student_id,
+        batch,
+        phone,
+        DOB,
+        email,
+        pwd,
+        imageUrl
+      } = req.body;
+  
+    
+      let student1 = await student.findOne({ email });
+    if (student1) {
+      throw new Error("User already exists");
+    }
+
+      student1 = {
+        name: name,
+        student_id: student_id,
+        batch: batch,
+        phone: phone,
+        DOB: DOB,
+        email: email,
+        pwd: pwd,
+        imageUrl: imageUrl
+      };
+  
+      const newstudent = new student(student1);
+      await newstudent.save();
+      const token = await newstudent.generateAuthToken();
+      res
+        .status(201)
+        .send({ status: "student Created", student: newstudent, token: token });
+        console.log(student1);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send({error: error.message});
+    }
+  });
+
+
+
+module.exports = router;
