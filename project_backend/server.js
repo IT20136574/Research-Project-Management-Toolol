327eb9194deaa12ebac09e5a059f659f528cd2bd
Ravindu Sandeepana <89466778+IT20136574@users.noreply.github.com>
2022-05-02 13:33:59 +0530

    serverjs update

--- project_backend/server.js
+++ project_backend/server.js
@@ -5,9 +5,17 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
 require("dotenv").config();
-
+app.use(express.json());
 
 const PORT = process.env.PORT || 8070;
+
+app.use(bodyParser.json({limit: '50mb'}) );
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true,
+  parameterLimit:50000
+}));
+app.use(cors());
 
 const URL = process.env.MONGODB_URL;
 process.env.SUPPRESS_NO_CONFIG_WARNING = 'y';
@@ -25,6 +33,18 @@ connection.once("open", () => {
 console.log("Mongodb connection success!!!");
 
 })
+
+// @import routes
+// const studentRouter = require("./routes/DH_routes/student");
+const staffRouter = require("./routes/RS_routes/staff");
+
+
+
+
+// rotues
+// app.use("/student",studentRouter);
+app.use("/staff",staffRouter);
+
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on port number: ${PORT}`)
