6d56fe1022a00fd41bbe243a2c8aecfcd625c35a
Dimalka Rathnayake <89465728+IT20128418@users.noreply.github.com>
2022-05-08 15:51:05 +0530

    signup component and app.js route

--- project_frontend/src/App.js
+++ project_frontend/src/App.js
@@ -3,12 +3,15 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
+import Signup from './components/DH_Components/student_Register';
+
+
 export default class App extends Component {
   render() {
     return (
         <Router>
             <Switch>
-              {/* <Route path="/" component={Sample} exact/> */}
+              <Route path ='/' component={Signup} />
             </Switch>
         </Router>
     )
